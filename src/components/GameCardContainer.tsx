7c84a424b20904b6fe5556d7c2d15349c1bba9d7
Israel Pedro <pedroisrael980@gmail.com>
2025-04-18 11:51:35 +0100

    Displaying the genre.

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -4,10 +4,12 @@ import { ReactNode } from "react";
 interface GameCardProps {
   children: ReactNode;
 }
-const GameCardComponent = ({children} : GameCardProps) => {
+const GameCardContainer = ({ children }: GameCardProps) => {
   return (
-    <Box width="300px" borderRadius={10}  overflow="hidden">{children}</Box>
+    <Box width="100%" borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
   );
 };
 
-export default GameCardComponent;
+export default GameCardContainer;

bffecf0ff2c784b8c71d85520dcd3cba4ae7a968
Israel Pedro <pedroisrael980@gmail.com>
2025-04-20 05:03:03 +0100

    Stored api key as environment variable.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL:'https://api.rawg.io/api', 
     params:{
-        key:'b93729931e1e41f7841a325577f7bf09'
+        key:import.meta.env.VITE_API_KEY
     }
 })
